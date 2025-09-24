41570a8c0984008618ccd2218c7d06ecbb56df2c
Raphael Reis <raphaelreisrodrigues@hotmail.com>
2025-09-24 20:11:31 -0300

    feat: adicionado hash nas senhas

--- src/core/useCases/user/createUser.useCase.ts
+++ src/core/useCases/user/createUser.useCase.ts
@@ -1,10 +1,16 @@
+import { IHash } from '../../../adapters/IHash';
 import { IUserRepository } from '../../../adapters/Repositories/IUserRepository';
 import { UserCreateDTO } from '../../Entities/UserEntity';
 
 export class CreateUserUseCase {
-    constructor(private repository: IUserRepository) {}
+    constructor(
+        private repository: IUserRepository,
+        private hash: IHash,
+    ) {}
 
     async execute(data: UserCreateDTO) {
+        data.password = await this.hash.hashPassword(data.password);
+
         return await this.repository.create(data);
     }
 }
--- src/factories/makeUserController.ts
+++ src/factories/makeUserController.ts
@@ -1,11 +1,13 @@
 import { UserController } from '../controllers/UserController';
 import { CreateUserUseCase } from '../core/useCases/user/createUser.useCase';
+import { HashBcrypt } from '../infra/hashBcrypt';
 import { UserRepositoryImpl } from '../infra/prisma/Repositories/UserRepositoryImpl';
 
 export function makeUserController() {
     const userRepo = new UserRepositoryImpl();
+    const hash = new HashBcrypt();
 
-    const createUser = new CreateUserUseCase(userRepo);
+    const createUser = new CreateUserUseCase(userRepo, hash);
 
     return new UserController(createUser);
 }
