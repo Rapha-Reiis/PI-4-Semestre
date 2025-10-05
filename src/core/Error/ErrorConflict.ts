3eaa2a57e5cc95277b1c9b99ccfef3ab84f7caf4
Raphael Reis <raphaelreisrodrigues@hotmail.com>
2025-10-05 14:59:16 -0300

    fix: ajuste no status code do ErrorConflict

--- src/core/Error/ErrorConflict.ts
+++ src/core/Error/ErrorConflict.ts
@@ -3,6 +3,6 @@ import { ErrorApp } from './ErrorApp';
 export class ErrorConflitct extends ErrorApp {
     constructor(details: unknown, msgText?: string) {
         const message = msgText ?? 'Conflito de dados';
-        super(message, 400, details);
+        super(message, 409, details);
     }
 }
