2fd7e3fc7a97c2a39fc063d9868ed916a5b7d94e
Raphael Reis <raphaelreisrodrigues@hotmail.com>
2025-10-23 07:31:51 -0300

    update: validação de status do game

--- src/core/useCases/userGame/userGame-create.usecase.ts
+++ src/core/useCases/userGame/userGame-create.usecase.ts
@@ -1,5 +1,6 @@
 import { IUserGameRepository } from '../../../adapters/Repositories/IuserGame-repository';
 import { ProfileCreateDTO } from '../../Entities/userGame-entity';
+import { ErrorBadRequest } from '../../Error/error-bad-request';
 import { ErrorConflitct } from '../../Error/error-conflict';
 
 export class UserGameCreateUsecase {
@@ -7,10 +8,16 @@ export class UserGameCreateUsecase {
 
     async exeute(data: ProfileCreateDTO) {
         const { gameId, userId } = data;
+        this.validate(data);
         const exist = await this.repository.VerifyGameWithUser(userId, gameId);
         if (exist) throw new ErrorConflitct('O jogo já cadastrado nesse perfil');
         const profileCreate = await this.repository.createUserProfile(data);
 
         return profileCreate;
+    }
+
+    validate(data: ProfileCreateDTO) {
+        if (!['BACKLOG', 'PLAYING', 'FINISHED', 'DROPPED'].includes(data.status))
+            throw new ErrorBadRequest('Parâmetro de status não é valido');
     }
 }
--- src/core/useCases/userGame/userGame-update.usecase.ts
+++ src/core/useCases/userGame/userGame-update.usecase.ts
@@ -1,10 +1,16 @@
 import { IUserGameRepository } from '../../../adapters/Repositories/IuserGame-repository';
 import { ProfileUpdateDTO } from '../../Entities/userGame-entity';
+import { ErrorBadRequest } from '../../Error/error-bad-request';
 
 export class UserGameUpdateUsecase {
     constructor(private repository: IUserGameRepository) {}
 
     async execute(data: ProfileUpdateDTO) {
         return await this.repository.UpdateDataProfile(data);
+    }
+
+    validate(data: ProfileUpdateDTO) {
+        if (!['BACKLOG', 'PLAYING', 'FINISHED', 'DROPPED'].includes(data.status))
+            throw new ErrorBadRequest('Parâmetro de status não é valido');
     }
 }
