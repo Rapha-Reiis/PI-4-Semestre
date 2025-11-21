d1b415ca550b54268e32a8b1aad38bbe46566de1
Raphael Reis <raphaelreisrodrigues@hotmail.com>
2025-11-20 21:17:17 -0300

    update: validação no token de email na implementação do JWT

--- src/adapters/IToken.ts
+++ src/adapters/IToken.ts
@@ -1,4 +1,5 @@
 export interface IToken {
     assin(userID: string): string;
+    signEmailToken(userId: string): string;
     verify(token: string): void;
 }
--- src/infra/JWT-token.ts
+++ src/infra/JWT-token.ts
@@ -1,25 +1,51 @@
 import { IToken } from '../adapters/IToken';
 import 'dotenv/config';
-import jwt from 'jsonwebtoken';
+import jwt, { TokenExpiredError } from 'jsonwebtoken';
 import { ErrorUnauthorized } from '../core/Error/error-unauthorized';
 import { ErrorBadRequest } from '../core/Error/error-bad-request';
+import { ErrorApp } from '../core/Error/erro-app';
 
 export class JwtToken implements IToken {
     private token = jwt;
-    private secret = process.env.SECRET_KEY;
+    private secretEmail: string = '';
+    private secretKey: string = '';
+    constructor() {
+        this.loadEnv();
+    }
+
+    loadEnv() {
+        this.secretEmail = process.env.EMAIL_TOKEN!;
+        this.secretKey = process.env.SECRET_KEY!;
+
+        if (!this.secretEmail) throw new ErrorApp('Não foi passado EMAIL_TOKEN, favor verificar .env', 500);
+        if (!this.secretKey) throw new ErrorApp('Não foi passado SECRET_KEY, favor verificar .env', 500);
+    }
 
     assin(userID: string): string {
-        const secret = process.env.SECRET_KEY ?? 'rapha';
-        return this.token.sign({ id: userID }, secret, {
+        return this.token.sign({ id: userID }, this.secretEmail, {
             expiresIn: '5d',
         });
     }
 
-    verify(token: string): void {
+    signEmailToken(userId: string): string {
+        const secret = process.env.EMAIL_TOKEN;
+        if (!secret) throw new ErrorBadRequest('Não foi passado EMAIL_TOKEN, verificar o .env');
+        return this.token.sign({ userId: userId }, secret, { expiresIn: '1d' });
+    }
+
+    verify(token: string, type?: 'email' | 'key'): any {
         try {
-            if (!this.secret) throw new ErrorBadRequest('Segredo não está definido no env');
-            this.token.verify(token, this.secret);
+            if (type && type == 'email') {
+                const payload = this.token.verify(token, this.secretEmail);
+
+                return payload;
+            } else {
+                this.token.verify(token, this.secretKey);
+            }
         } catch (err) {
+            if (err instanceof TokenExpiredError) {
+                throw new ErrorBadRequest('Token expirado');
+            }
             throw new ErrorUnauthorized('Token não autorizado');
         }
     }
